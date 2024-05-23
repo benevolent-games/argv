145ad0872d329528f705f65e550dc39f77f2b8da
Chase Moskal <chasemoskal@gmail.com>
2024-05-23 01:48:38 -0700

    cleanup: errors

--- s/errors/basic.ts
+++ s/errors/basic.ts
@@ -14,8 +14,34 @@ export class ArgvError extends Error {
 export class ConfigError extends ArgvError {}
 
 /** user error in supplying command line inputs. */
-export class MistakeError extends ArgvError {}
+export class MistakeError extends ArgvError {
+	constructor(message: string) {
+		super(`${message}, ${pleaseSeekHelp()}`)
+	}
+}
 
 /** cli is expeceted to throw this during testing scenarios. */
 export class FakeExit extends ArgvError {}
+
+// i just think this is fun
+function pleaseSeekHelp() {
+	const choices = [
+		`seek --help`,
+		`please seek --help`,
+		`try seeking --help`,
+		`consider seeking --help`,
+
+		`ask for --help`,
+		`please ask for --help`,
+		`try asking for --help`,
+		`consider asking for --help`,
+		`it's okay to ask for --help`,
+
+		`you need --help`,
+		`get some --help`,
+		`get --help`,
+		`please get --help`,
+	]
+	return choices[Math.floor(Math.random() * choices.length)]
+}
 
