48914a5e2aa51a4f82a0ca8973526d4dbb3dd332
Chase Moskal <chasemoskal@gmail.com>
2024-05-23 21:28:45 -0700

    organize: analyize-command, analyze-tree

--- s/areas/analysis/utils/utils.ts
+++ s/areas/analysis/utils/utils.ts
@@ -1,31 +1,8 @@
 
 import {parse} from "../../parsing/parse.js"
-import {Parsed} from "../../parsing/types.js"
-import {MistakeError} from "../../../errors/basic.js"
+import {SelectedCommand} from "../types/analysis.js"
+import {listAllCommands} from "./list-all-commands.js"
 import {Command, CommandTree} from "../types/commands.js"
-import {CommandAnalysis, SelectedCommand, TreeAnalysis} from "../types/analysis.js"
-import { listAllCommands } from "./list-all-commands.js"
-
-export function produceTreeAnalysis<C extends CommandTree>(
-		commands: C,
-		command: Command,
-		commandAnalysis: CommandAnalysis<Command>,
-	): TreeAnalysis<C> {
-
-	function recurse(c: CommandTree, path: string[]): any {
-		if (c instanceof Command)
-			return (c === command)
-				? commandAnalysis
-				: undefined
-		else
-			return Object.fromEntries(
-				Object.entries(c)
-					.map(([key, c2]) => [key, recurse(c2, [...path, key])])
-			)
-	}
-
-	return recurse(commands, [])
-}
 
 export function listRelevantCommands(argw: string[], commands: CommandTree) {
 	return listAllCommands(commands)
@@ -49,50 +26,6 @@ export function selectCommand(argw: string[], commands: CommandTree) {
 	return recurse(commands, [])
 }
 
-export function analyzeCommand(
-		path: string[],
-		command: Command,
-		parsed: Parsed,
-	): CommandAnalysis<Command> {
-
-	function handleError<T>(subject: string, name: string, fn: () => T) {
-		try { return fn() }
-		catch (error) {
-			const message = (error instanceof Error)
-				? error.message
-				: "unknown error"
-			throw new MistakeError(`${subject} ${name} ${message}`)
-		}
-	}
-
-	const args = Object.fromEntries(
-		command.args.map((arg, index) => {
-			const input = parsed.args.at(index)
-			return handleError("arg", `"${arg.name}"`, () =>
-				[arg.name, arg.ingest(input)]
-			)
-		})
-	)
-
-	const params = Object.fromEntries(
-		Object.entries(command.params)
-			.map(([name, param]) => {
-				if (param.flag && parsed.flags.has(param.flag))
-					return [name, true]
-				const input = parsed.params.get(name)
-				return handleError("param", `--${name}`, () =>
-					[name, param.ingest(input)]
-				)
-			})
-	)
-
-	const extraArgs = (parsed.args.length > command.args.length)
-		? parsed.args.slice(command.args.length)
-		: []
-
-	return {path, args, params, extraArgs}
-}
-
 export function getBooleanParams(command: Command) {
 	return Object.entries(command.params)
 		.filter(([,param]) => (
@@ -103,8 +36,7 @@ export function getBooleanParams(command: Command) {
 		.map(([name]) => name)
 }
 
-///////////////////////////
-///////////////////////////
+//////////////////
 
 function isCommandMatching(argw: string[], path: string[]) {
 	const {args} = parse(argw, {booleanParams: ["help"]})
